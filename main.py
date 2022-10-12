97c60be534121424a967db11eadceba6bf6e4059
길가던행자 <cdaosldk@naver.com>
2022-10-12 08:49:28 +0000

    22/10/12

--- main.py
+++ main.py
@@ -1,32 +1,10 @@
 from requests import get
 
-websites = (
-  "google.com",
-  "airbnb.com",
-  "twitter.com",
-  "facebook.com",
-  "tiktok.com"
-)
-
-results= {}
-
-#매 번 반복문이 실행될 때마다, list에서 each 매개변수로 값을 저장 후 실행
-for website in websites:
-  if not website.startswith("https://"):
-    website = f"https://{website}"
-    response=get(website)
-    print(response.status_code)
-    if response.status_code >= 200:
-      results[websites] = "ok"
-    elif response.status_code >= 100:
-      results[websites] = "information response"
-    elif response.status_code >= 300:
-      results[websites] = "redirection message"
-    elif response.status_code >= 400:
-      results[websites] = "client error response"
-    elif response.status_code >= 500:
-      results[websites] = "server error response"
-      
-print(results)
-#String에 변수를 추가하기: f""
-#response 200은 웹사이트가 성공적으로 응답했다는 뜻
+base_url = "https://weworkremotely.com/remote-jobs/search?term="
+search_term = "python"
+response = get(f"{base_url}{search_term}")
+if response.status_code != 200:
+  print("Can't request website")
+else:
+  f response.status_code != 200:
+  print()print(response.text)
