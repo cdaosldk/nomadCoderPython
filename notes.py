c7df539ccc964e5e1c138994f9b6fd74397d96a6
길가던행자 <cdaosldk@naver.com>
2022-10-12 01:40:17 +0000

    22/10/12

--- notes.py
+++ notes.py
@@ -175,17 +175,3 @@ player.pop('age')
 player['xp'] = 1500
 """
 #키 값으로는 문자열, 참거짓, 숫자, 리스트, 튜플이 사용가능
-
-websites = (
-  "google.com",
-  "https://airbnb.com",
-  "https://twitter.com",
-  "https://facebook.com",
-  "https://tiktok.com"
-)
-#매 번 반복문이 실행될 때마다, list에서 each 매개변수로 값을 저장 후 실행
-for website in websites:
-  if not website.startswith("https://"):
-    print("have to fix")
-
-#String에 변수를 추가하기: f""
